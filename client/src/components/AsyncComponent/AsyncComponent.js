07934d554c4e5fe132f4598c1847f3d2f08ee902
Irina <irina.machado@ontargetech.com>
2020-02-19 14:01:52 -0600

    references fixed

--- client/src/components/AsyncComponent/AsyncComponent.js
+++ client/src/components/AsyncComponent/AsyncComponent.js
@@ -6,34 +6,17 @@ import React from 'react';
 import Loadable from 'react-loadable';
 
 // rct page loader
-import RctPageLoader from 'components/RctPageLoader/RctPageLoader';
+import RctPageLoader from '../../components/RctPageLoader/RctPageLoader';
 
 // ecommerce dashboard
 const AsyncEcommerceDashboardComponent = Loadable({
-   loader: () => import("routes/dashboard/ecommerce"),
+   loader: () => import("../../routes/dashboard/ecommerce"),
    loading: () => <RctPageLoader />,
 });
 
-// agency dashboard
-const AsyncSaasDashboardComponent = Loadable({
-   loader: () => import("routes/dashboard/saas"),
-   loading: () => <RctPageLoader />,
-});
-
-// agency dashboard
-const AsyncAgencyDashboardComponent = Loadable({
-   loader: () => import("routes/dashboard/agency"),
-   loading: () => <RctPageLoader />,
-});
-
-// boxed dashboard
-const AsyncNewsDashboardComponent = Loadable({
-   loader: () => import("routes/dashboard/news"),
-   loading: () => <RctPageLoader />,
-});
 // crm dashboard
 const AsyncCrmComponent = Loadable({
-   loader: () => import("routes/crm/dashboard"),
+   loader: () => import("../../routes/crm/dashboard"),
    loading: () => <RctPageLoader />,
 });
 export {
