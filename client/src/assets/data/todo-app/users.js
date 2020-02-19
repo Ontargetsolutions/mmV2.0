3cfa2ea9ee1e98f3601292c511ef6567f1a70154
Irina <irina.machado@ontargetech.com>
2020-02-19 13:43:56 -0600

    references

--- client/src/assets/data/todo-app/users.js
+++ client/src/assets/data/todo-app/users.js
@@ -2,21 +2,21 @@ export default [
     {
         id: 5,
         name: 'John Doe',
-        photo_url: require('assets/avatars/user-2.jpg')
+        photo_url: require('../../avatars/user-2.jpg')
     },
     {
         id: 6,
         name: 'Smith Lee',
-        photo_url: require('assets/avatars/user-3.jpg')
+        photo_url: require('../../avatars/user-3.jpg')
     },
     {
         id: 7,
         name: 'Lisa Roy',
-        photo_url: require('assets/avatars/user-4.jpg')
+        photo_url: require('../../avatars/user-4.jpg')
     },
     {
         id: 8,
         name: 'Lusile Beck',
-        photo_url: require('assets/avatars/user-5.jpg')
+        photo_url: require('../../avatars/user-5.jpg')
     }
 ];
