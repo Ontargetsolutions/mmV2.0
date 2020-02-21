987cc3352ec067120dbf055d9bff920b7f906bb3
Irina <irina.machado@ontargetech.com>
2020-02-21 09:04:29 -0600

    save used fixed

--- config/config.js
+++ config/config.js
@@ -18,10 +18,10 @@ module.exports = {
     operatorsAliases: false,
   },
   production: {
-    username: process.env.DB_USERNAME ||'root',
-    password: process.env.DB_PASSWORD ||'root',
-    database: process.env.DB_DATABASE ||'mmquote_prod',
-    host: process.env.DB_HOST ||'127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     operatorsAliases: false,
   }
